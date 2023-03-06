060ad592f5c5d3f6c98af579ecf1592e100984ff
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-03-06 15:24:34 -0300

    updated button styling from previous commit

--- src/components/ButtonCheckBox.tsx
+++ src/components/ButtonCheckBox.tsx
@@ -1,4 +1,6 @@
 import classNames from 'classnames'
+import { X, Circle } from 'phosphor-react'
+
 
 interface ButtonCheckBoxProps {
 	arrayValue: string,
@@ -17,7 +19,7 @@ export function ButtonCheckBox({ arrayValue, type, key, arrayIndex, index, array
 	return (
 		<>
 			<button type="button" key={key + 97} className={classNames("flex justify-center items-center w-6 h-6 rounded pointer", {
-				'bg-gray-400 border-2 border-gray-200 hover:bg-gray-600 hover:border-gray-400 transition': arrayValue == "0",
+				'bg-gray-500 border-2 border-gray-500 hover:bg-gray-600 hover:border-gray-600 transition': arrayValue == "0",
 				'bg-red-400 border-2 border-red-400 hover:bg-red-500 hover:border-red-500 transition': physicalDamage,
 				'bg-purple-400 border-2 border-purple-400 hover:bg-purple-500 hover:border-purple-500 transition': psychological
 
@@ -34,7 +36,7 @@ export function ButtonCheckBox({ arrayValue, type, key, arrayIndex, index, array
 					setArray(copyArray)
 				})}
 			>
-				{arrayValue}
+				{arrayValue === "X" ? <X fill="bold" /> : <Circle fill="bold" />}
 			</button>
 		</>
 	)
