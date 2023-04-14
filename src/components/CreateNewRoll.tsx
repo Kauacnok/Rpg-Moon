cec36a509ca9ae60b445dfed11dbb2658830893b
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-04-14 14:21:57 -0300

    Intercepted the result of the dice roll to display it as quickly as possible to the user in a system message. While the database tries to receive the new changes

--- src/components/CreateNewRoll.tsx
+++ src/components/CreateNewRoll.tsx
@@ -20,9 +20,12 @@ export function CreateNewRoll({ VITE_API_URL, VITE_API_ACCESS_TOKEN }: createNew
 	const [refreshTokenApi, setRefreshTokenApi] = useState(Math.random());
 	const [isActiveFireworks, setIsActiveFireworks] = useState(false)
 	const [name, setName] = useState("...")
+	const [resultString, setResultString] = useState("")
+	const [totalNumberResult, setTotalNumberResult] = useState(0)
 	const [aditionalNumber, setAditionalNumber] = useState(0)
 	const [isDataSent, setIsDataSent] = useState(false)
 	const [isFireworksEnabled, setIsFireworksEnabled] = useState(false)
+	const [showSystemMessage, setShowSystemMessage] = useState(false)
 
 	const client = useApolloClient()
 
@@ -51,6 +54,10 @@ export function CreateNewRoll({ VITE_API_URL, VITE_API_ACCESS_TOKEN }: createNew
     	return Math.round(Math.random() * (max - min) + min);
 	}
 
+	function closeSystemMessage() {
+		setShowSystemMessage(false)
+	}
+
 	async function handleSubmit(event: FormEvent) {
 		event.preventDefault()
 		setIsDataSent(true)
@@ -63,6 +70,12 @@ export function CreateNewRoll({ VITE_API_URL, VITE_API_ACCESS_TOKEN }: createNew
 		const totalNumberResultOnlyDice = rollDiceNumber[randomNumber1] + rollDiceNumber[randomNumber2] + rollDiceNumber[randomNumber3] + rollDiceNumber[randomNumber4]
 		const resultDiceStringFull = `${rollDiceString[randomNumber1]} ${rollDiceString[randomNumber2]} ${rollDiceString[randomNumber3]} ${rollDiceString[randomNumber4]}`
 		const totalNumberResultFull = rollDiceNumber[randomNumber1] + rollDiceNumber[randomNumber2] + rollDiceNumber[randomNumber3] + rollDiceNumber[randomNumber4] + Number(aditionalNumber)
+		setResultString(resultDiceStringFull)
+		setTotalNumberResult(totalNumberResultFull)
+		setShowSystemMessage(true)
+		setTimeout(() => {
+			setShowSystemMessage(false)
+		}, 15000)
 
 		const newRollDice = await graphQLClient.request(createNewRollMutation, {
 			player: name,
@@ -82,7 +95,7 @@ export function CreateNewRoll({ VITE_API_URL, VITE_API_ACCESS_TOKEN }: createNew
 				await fetch('https://rpg-moon-simple-api.onrender.com/fireworks/false')
 				ref?.current?.stop()
 
-			}, 15000)
+			}, 10000)
 		}
 
         setIsDataSent(false)
@@ -103,6 +116,14 @@ export function CreateNewRoll({ VITE_API_URL, VITE_API_ACCESS_TOKEN }: createNew
 						background: 'transparent'
 					}}
      			 />
+			}
+			{showSystemMessage && 
+				<div className="fixed bg-gray-700 w-[100%] rounded top-4 px-8 py-10 flex flex-col justify-center items-center text-center border-[2px] border-gray-500 gap-5">
+					<div>
+						Mensagem do sistema: Você tirou <span className="text-blue-500">[ {resultString} ] ({aditionalNumber})</span> e o resultado total foi <span className="text-blue-500">{totalNumberResult}</span>	
+					</div>
+					<button type="button" className="rounded px-5 py-2 bg-green-500 cursor-pointer hover:bg-green-700 transition" onClick={closeSystemMessage}>Fechar mensagem</button>
+				</div>
 			}
 			<form onSubmit={handleSubmit} className="bg-gray-700 flex flex-col pl-2 pb-2">
 				<InputForm typeInput="text" placeholderInput="Nome do jogador" setValue={setName}/>
